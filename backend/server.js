fb9148a752ef1a6e035a2ac8c61cb4bc1e41b1c9
Ubuntu <will.clark@duke.edu>
2019-03-22 00:31:09 +0000

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,16 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const slaesRoutes = express.Router();
 const PORT = 4000;
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const dbConnection = require('./db') // loads our connection to the mongo database
+const passport = require('./passport')
+const app = express()
+
 
 let Slaes = require('./data');
 
@@ -68,6 +78,77 @@ slaesRoutes.route('/add').post(function(req, res) {
 });
 
 app.use('/slaes', slaesRoutes);
+
+// Loading evnironmental variables here
+if (process.env.NODE_ENV !== 'production') {
+	console.log('loading dev environments')
+	require('dotenv').config()
+}
+
+// ===== Middleware ====
+app.use(morgan('dev'))
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
+app.use(
+	session({
+		secret: process.env.APP_SECRET || 'this is the default passphrase',
+		store: new MongoStore({ mongooseConnection: dbConnection }),
+		resave: false,
+		saveUninitialized: false
+	})
+)
+
+// ===== Passport ====
+app.use(passport.initialize())
+app.use(passport.session()) // will call the deserializeUser
+
+// ===== testing middleware =====
+// app.use(function(req, res, next) {
+// 	console.log('===== passport user =======')
+// 	console.log(req.session)
+// 	console.log(req.user)
+// 	console.log('===== END =======')
+// 	next()
+// })
+// testing
+// app.get(
+// 	'/auth/google/callback',
+// 	(req, res, next) => {
+// 		console.log(`req.user: ${req.user}`)
+// 		console.log('======= /auth/google/callback was called! =====')
+// 		next()
+// 	},
+// 	passport.authenticate('google', { failureRedirect: '/login' }),
+// 	(req, res) => {
+// 		res.redirect('/')
+// 	}
+// )
+
+// ==== if its production environment!
+if (process.env.NODE_ENV === 'production') {
+	const path = require('path')
+	console.log('YOU ARE IN THE PRODUCTION ENV')
+	app.use('/static', express.static(path.join(__dirname, '../build/static')))
+	app.get('/', (req, res) => {
+		res.sendFile(path.join(__dirname, '../build/'))
+	})
+}
+
+/* Express app ROUTING */
+app.use('/auth', require('./auth'))
+
+// ====== Error handler ====
+app.use(function(err, req, res, next) {
+	console.log('====== ERROR =======')
+	console.error(err.stack)
+	res.status(500)
+})
+
+// ==== Starting Server =====
 
 // launch our backend into a port
 app.listen(PORT, function() {
